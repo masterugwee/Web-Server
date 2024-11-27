457fb3f4159f53ff498479855793df95bbbd70b2
masterugwee <varun199700@gmail.com>
2024-11-27 14:26:54 +0530

    - Added comments to server.c

--- src/server.c
+++ src/server.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/wait.h>
-#include "server.h"
-#include "request_handler.h"
+#include <stdio.h> // Common functions, print, scan, open, read
+#include <stdlib.h> // exit, memory
+#include <string.h> // string handling
+#include <unistd.h> // fork()
+#include <arpa/inet.h>  // internet operations, ip addresses
+#include <sys/wait.h>  // Process term and wait
+#include "server.h" // User-defined header-files
+#include "request_handler.h" 
 
 #define PORT 1337
 #define BACKLOG 10
@@ -14,23 +14,28 @@ void start_server() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
+// Sockets are endpoints for send and receiving data over network
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+// The condition checks if socket creation failed, IPv4, TCP, 0 - defaults to TCP. 
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_family = AF_INET;   // Specifies IPv4 addresses
+    address.sin_addr.s_addr = INADDR_ANY; // Any incoming interfaces
+    address.sin_port = htons(PORT);     // Port
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        // binds sockets to IP addresses
+        perror("bind failed"); 
         close(server_fd);
+        // releases the resources associated with socket
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, BACKLOG) < 0) {
+        // Listen to incoming connections 
         perror("listen");
         close(server_fd);
         exit(EXIT_FAILURE);
@@ -39,7 +44,9 @@ void start_server() {
     printf("Server started on port %d\n", PORT);
 
     while (1) {
+        //Infinite loop for accepting connections
         new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        // Waits for new connection, if connected creates a new socket
         if (new_socket < 0) {
             perror("accept");
             continue;
@@ -48,10 +55,10 @@ void start_server() {
         pid_t pid = fork();
         if (pid == 0) {
             // Child process
-            close(server_fd);
-            handle_request(new_socket);
-            close(new_socket);
-            exit(0);
+            close(server_fd); // No need to accept any further connection, closes the fd
+            handle_request(new_socket); // Sends it to handle_request
+            close(new_socket); // Closes the socket after handling the request
+            exit(0); // Finally terminates the child process
         } else if (pid < 0) {
             perror("fork");
         } else {
